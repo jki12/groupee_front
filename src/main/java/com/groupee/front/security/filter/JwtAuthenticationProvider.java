f411ce0c07cd06b923e391670c463829552eb024
jki12 <sjak0227@naver.com>
2024-07-01 17:37:50 +0900

    feat: JwtAuthenticationProvider 추가

---
+++ src/main/java/com/groupee/front/security/filter/JwtAuthenticationProvider.java
@@ -0,0 +1,52 @@
+package com.groupee.front.security.filter;
+
+import com.groupee.front.exception.InvalidJwtException;
+import com.groupee.front.security.JwtAuthenticationToken;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Component
+public class JwtAuthenticationProvider implements AuthenticationProvider {
+    private final JwtParser jwtParser;
+
+    private List<GrantedAuthority> createAuthorities(List<String> authorities) {
+        Assert.notNull(authorities, "authorities는 절대 null이 될 수 없습니다.");
+
+        return authorities.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) authentication; // supports에서 JwtAuthenticationToken 검증.
+
+        try {
+            Claims jwt = jwtParser.parseClaimsJws(jwtAuthenticationToken.getJwt()).getBody();
+
+            return new JwtAuthenticationToken(jwt.get("id"), "", createAuthorities((List<String>) jwt.get("authorities")));
+        } catch (ExpiredJwtException | MalformedJwtException | SignatureException | IllegalArgumentException e) {
+            throw new InvalidJwtException(e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return JwtAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
